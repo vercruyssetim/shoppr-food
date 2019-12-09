3d26891cc923e651b84a578f27723f147ff8ffc6
vercruyssetim <tim.vercruysse@cegeka.be>
2019-12-09 10:53:47 +0100

    ignoring case in query

--- src/main/java/com/switchfully/shoppr/product/ProductRepository.java
+++ src/main/java/com/switchfully/shoppr/product/ProductRepository.java
@@ -2,12 +2,14 @@ package com.switchfully.shoppr.product;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
-    List<Product> findByNameContaining(@Param("name") String name);
+    @RestResource(path = "findByName", rel = "findByName")
+    List<Product> findByNameContainingIgnoreCase(@Param("name") String name);
 
     List<Product> findByProductType(@Param("productType") ProductType productType);
 }
