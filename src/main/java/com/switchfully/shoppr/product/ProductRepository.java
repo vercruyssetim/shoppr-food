d852b21278ee6bd8d422d67d2624dd165a73782f
vercruyssetim <tim.vercruysse@cegeka.be>
2019-12-11 15:46:44 +0100

    added cors config

--- src/main/java/com/switchfully/shoppr/product/ProductRepository.java
+++ src/main/java/com/switchfully/shoppr/product/ProductRepository.java
@@ -3,11 +3,9 @@ package com.switchfully.shoppr.product;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
-@CrossOrigin
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
     @RestResource(path = "findByName", rel = "findByName")
