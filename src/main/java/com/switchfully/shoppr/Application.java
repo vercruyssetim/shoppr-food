628f5ffea4d13d966a6dee4f0bd2764eeb1aac5e
vercruyssetim <tim.vercruysse@cegeka.be>
2019-12-11 16:14:51 +0100

    added cors config

--- src/main/java/com/switchfully/shoppr/Application.java
+++ src/main/java/com/switchfully/shoppr/Application.java
@@ -2,9 +2,14 @@ package com.switchfully.shoppr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -14,9 +19,11 @@ public class Application {
     }
 
     @Bean
-    UrlBasedCorsConfigurationSource corsFilter() {
+    FilterRegistrationBean corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
